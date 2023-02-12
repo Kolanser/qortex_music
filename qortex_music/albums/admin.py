f675b43dcfc07c7c1fdea86928d9120f0d7f2246
Николай Слесарев <kolansers@yandex.ru>
2023-02-12 16:44:47 +0300

    Добавлена админка

--- qortex_music/albums/admin.py
+++ qortex_music/albums/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album, AlbumSing, Sing, Singer
+
+
+@admin.register(AlbumSing)
+class AlbumSing(admin.ModelAdmin):
+    """Админка для связи альбомов и песен."""
+    list_display = (
+        'album',
+        'sing',
+        'number_sing',
+    )
+    search_fields = ('sing',)
+    list_filter = ('album',)
+
+
+@admin.register(Album)
+class Album(admin.ModelAdmin):
+    """Админка для альбомов."""
+    list_display = (
+        'name',
+        'year',
+        'singer',
+    )
+    search_fields = ('name',)
+    list_filter = ('year', 'singer')
+
+
+@admin.register(Sing)
+class Album(admin.ModelAdmin):
+    """Админка для альбомов."""
+    list_display = (
+        'name',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(Singer)
+class Album(admin.ModelAdmin):
+    """Админка для альбомов."""
+    list_display = (
+        'name',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
