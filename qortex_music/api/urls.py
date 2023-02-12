b75b5b1280acbd69e0889056bdab17d89f965cc3
Николай Слесарев <kolansers@yandex.ru>
2023-02-12 16:19:57 +0300

    Добавлены urls для эндпоинтов песни, исполнители и альбомы

--- qortex_music/api/urls.py
+++ qortex_music/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import (
+    AlbumViewSet,
     SingViewSet,
     SingerViewSet
 )
 
 router = routers.DefaultRouter()
 router.register('sings', SingViewSet, basename='sing')
-router.register('singers', SingerViewSet, basename='singers')
+router.register('singers', SingerViewSet, basename='singer')
+router.register('albums', AlbumViewSet, basename='album')
 
 urlpatterns = [
     path('', include(router.urls)),
