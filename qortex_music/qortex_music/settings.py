863824e76f495a943620fac1acabce54c9ff44da
Николай Слесарев <kolansers@yandex.ru>
2023-02-13 23:34:07 +0300

    Создан файл пакетов

--- qortex_music/qortex_music/settings.py
+++ qortex_music/qortex_music/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_53m19%p#44ut8_sa6)q)o-_+z=5komgmy2taro3ey2qvi%@t^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
@@ -122,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
